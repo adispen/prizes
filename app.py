10879a225a35739c81549e55ae11c99e285e1538
Aedan Dispenza <aedan.dispenza@gmail.com>
2015-05-01 21:23:35 -0400

    Functional, updating every 3 seconds

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 import urllib, json
 import locale
 
 locale.setlocale(locale.LC_ALL, '')
 url = "https://api.steampowered.com/IEconDOTA2_570/GetTournamentPrizePool/v1/?key=66E1E5D64EE0F14B06F3CBE57F75ABDA&leagueid=2733"
-response = urllib.urlopen(url)
-data = json.loads(response.read())
-prize = data['result']['prize_pool']
+app = Flask(__name__)
 
-print locale.currency(prize, grouping=True)
+def getJson():
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+	prize = locale.currency(data['result']['prize_pool'], grouping=True)
+	return prize
+
+@app.route('/')
+def show_prize(prize=None):
+	return render_template('index.html', prize=getJson())
+
+@app.route('/update', methods=['GET'])
+def update():
+	return json.dumps({'prize': getJson()})
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
